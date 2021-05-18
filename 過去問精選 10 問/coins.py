c708a2aa77a17ed0a014039d544746999ac8850d
tsuruhisayukina <tsuruhisayukina.0110@icloud.com>
2021-05-18 10:53:21 +0900

    B - Coins

---
+++ 過去問精選 10 問/coins.py
@@ -0,0 +1,12 @@
+A = int(input())
+B = int(input())
+C = int(input())
+X = int(input())
+cnt = 0
+for a in range(A+1):
+    for b in range(B+1):
+        for c in range(C+1):
+            money = 500 * a + 100 * b + 50 * c
+            if money == X:
+                cnt += 1
+print(cnt)
