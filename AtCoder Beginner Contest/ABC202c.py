7b7765039c4f84390eefedc77a2e85a83b8860db
tsuruhisayukina <tsuruhisayukina.0110@icloud.com>
2021-05-23 21:30:40 +0900

    ABC202

--- AtCoder Beginner Contest/ABC202c.py
+++ AtCoder Beginner Contest/ABC202c.py
@@ -1,9 +1,11 @@
+from collections import Counter
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 C = list(map(int, input().split()))
 cnt = 0
-for i in range(N):
-    if B[i] in A and (B.index(B[i]) + 1) in C:
-        cnt += 1
-print(cnt**2)
+A_cnt = Counter(A)
+for j in range(N):
+    Dj = B[C[j]-1]
+    cnt += A_cnt[Dj]
+print(cnt)
